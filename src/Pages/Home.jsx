7e38e20e11d74325d3a6f9ea0d0c5feb9c23371c
Abhishek Singh <singhabhishek111213@gmail.com>
2024-05-02 16:37:33 +0530

    Added home page UI

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,29 @@
-import Footer from "Components/Footer/Footer";
-
+import Logo from "Assets/Images/logo-color.png";
 export default function Home() {
   return (
-    <div>
-      Home <Footer />
+    <div className="flex flex-col items-center justify-center gap-20 h-[100vh]">
+      <div className="h-48 w-48">
+        <img className="w-full h-full" alt="logo" src={Logo} />
+      </div>
+      <div className="flex justify-around items-center gap-16">
+        <div className="w-2/4 text-center font-semibold basis-1/2">
+          <h1 className="text-white text-5xl">
+            BookShelf
+            <br />
+            <span className="text-warning tracking-widest leading-normal">
+              Your personal library and social network for bookworms
+            </span>
+          </h1>
+        </div>
+        <div>
+          <button className="btn btn-primary rounded-md px-5 py-2 text-xl">
+            Register
+          </button>
+          <button className="btn btn-warning rounded-md mx-3 px-5 py-2 text-xl">
+            Login
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
